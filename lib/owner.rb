63fdefabba56f045516b2457ac74a83166521a96
babyshoes <dianecai@gmail.com>
2015-08-09 15:25:52 -0400

    all done

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code your solution here!
+
+  attr_reader :name, :dog
+
+  def initialize(name, dog)
+    @name = name
+    @dog = dog
+  end
+
+  def prepare_items
+    dog.leash = true
+    dog.plastic_bag = true
+  end
+
+  def walk
+    walking? if prepare_items
+    
+  end
+
+  def walking?
+    dog.walking?(self)
+  end
+
+  def vet_checkup
+    dog.vet_checkup?(self) if prepare_items
+  end
+
 end
